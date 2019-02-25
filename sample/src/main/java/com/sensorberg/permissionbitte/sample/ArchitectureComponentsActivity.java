6796ecbc971d381090ecb59e68d9c3acd06071fb
Mirko Häberlin <mirko.haeberlin@sensorberg.com>
2019-02-25 16:25:18 +0100

    androidx migration

--- sample/src/main/java/com/sensorberg/permissionbitte/sample/ArchitectureComponentsActivity.java
+++ sample/src/main/java/com/sensorberg/permissionbitte/sample/ArchitectureComponentsActivity.java
@@ -1,12 +1,12 @@
 package com.sensorberg.permissionbitte.sample;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
